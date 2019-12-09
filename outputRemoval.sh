90f94a4b9811290ef679fb95f9e56fdc49796461
pcamach2 <49655443+pcamach2@users.noreply.github.com>
2019-12-09 16:32:56 -0600

    Create outputRemoval.sh

---
+++ outputRemoval.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+WorkDir=/path/to/dataDirectory
+
+#cleans out all results of pipeline_example except for dcm2niix outputs
+for SUB in 001 002 003 004 005
+do
+	cd ${WorkDir}/${SUB}/Analyze/unbiased
+    rm ./*
+	cd ${WorkDir}/${SUB}/Analyze/bet_notunbiased
+	rm ./*
+    cd ${WorkDir}/${SUB}/Analyze/lesions_no2fast
+	rm ./*
+    cd ${WorkDir}/${SUB}/Analyze/bet_multispectral
+	rm ./*
+    cd ${WorkDir}/${SUB}/Analyze/sienax_no2fast
+    rm ./*
+done
